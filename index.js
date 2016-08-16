cbfccfdb4a74d79ffee319550915944c1bf6d9a4
小奔奔 <bunwong@qq.com>
2016-08-16 18:27:51 +0800

    1.1.4

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = {
   init: (o) => {
     options = o;
   },
-  start: (next) => {
+  start: (next, onMasterExit) => {
     options.config = options.config || 'etc/config.js';
     options.path = options.path || 'sys';
-    server = require('./lib/core.js')(options, services, next);
+    server = require('./lib/core.js')(options, services, next, onMasterExit);
   },
   use: (serviceName, instance) => services[serviceName] = instance || null
 };
